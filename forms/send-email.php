56e9c7a31c96249135039232db3afa90051f88bb
clm881142 <chiulapman00@gmail.com>
2024-12-01 06:41:14 +0800

    Update send-email.php

--- forms/send-email.php
+++ forms/send-email.php
@@ -1,27 +1,36 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'assets/vendor/phpmailer/src/Exception.php';
+require 'assets/vendor/phpmailer/src/PHPMailer.php';
+require 'assets/vendor/phpmailer/src/SMTP.php';
+
+
 
 $name = $_POST["name"];
 $email = $_POST["email"];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
 
-require dirname(__FILE__).'/vendor/autoload.php';    
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
-$mail = new PHPMailer(true); 
+
+
+
+$mail = new PHPMailer(true);
 
 // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
 $mail->isSMTP();
 $mail->SMTPAuth = true;
 
-$mail->Host = "smtp.example.com";
+$mail->Host = "smtp.gmail.com";
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail->Port = 587;
 
 $mail->Username = "ericchiu456@gmail.com";
-$mail->Password = "xdumhdneqfsyluam";
+$mail->Password = "xdumhdneqfsyluam";    
 
 $mail->setFrom($email, $name);
 $mail->addAddress("ericchiu456@gmail.com", "Eric");
